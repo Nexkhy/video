dba40c383c3fbc4bf6b13455ca932970bf20814a
nelson-siebi <nelsonsiebi@gmail.com>
2026-04-14 03:48:55 +0100

    g

--- admin2/model/Database.php
+++ admin2/model/Database.php
@@ -1,27 +1,29 @@
 <?php
 // Gère la connexion à la base de données MySQL via PDO
-class Database {
+class Database
+{
     private $dsn;
     private $username;
     private $password;
     private $pdo;
 
 
-    public function __construct() {
-        $this->dsn= 'mysql:host=localhost;dbname=ecosantedb;port=3306;charset=utf8';
-        $this->username= 'root';
-        $this->password= '';
+    public function __construct()
+    {
+        $this->dsn = 'mysql:host=mysql-princesse.alwaysdata.net;dbname=princesse_db1;port=3306;charset=utf8';
+        $this->username = 'princesse_db1';
+        $this->password = 'nexky237';
     }
 
-    
-    public function getConnect() {
-        if($this->pdo === null) {
+
+    public function getConnect()
+    {
+        if ($this->pdo === null) {
             try {
-                $this->pdo= new PDO($this->dsn, $this->username, $this->password);
+                $this->pdo = new PDO($this->dsn, $this->username, $this->password);
                 $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-            }
-            catch(Exception $ex) {
+            } catch (Exception $ex) {
                 die('Echec de connexion : ' . $ex->getMessage());
             }
         }
@@ -29,27 +31,27 @@ class Database {
     }
 
 
-    public function prepare($sql, $params= null) {
-        $req= $this->getConnect()->prepare($sql);
-        if(is_null($params)) {
+    public function prepare($sql, $params = null)
+    {
+        $req = $this->getConnect()->prepare($sql);
+        if (is_null($params)) {
             $req->execute();
-        }
-        else {
+        } else {
             $req->execute($params);
         }
         return $req;
     }
 
 
-    public function getDatas($req, $one= true) {
-        $datas= null;
-        if($one == true) {
-            $datas= $req->fetch();
+    public function getDatas($req, $one = true)
+    {
+        $datas = null;
+        if ($one == true) {
+            $datas = $req->fetch();
+        } else {
+            $datas = $req->fetchAll();
         }
-        else {
-            $datas= $req->fetchAll();
-        }
-        return $datas; 
+        return $datas;
     }
 }
 
